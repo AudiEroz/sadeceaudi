83dbf7b34ec50319bd1f059f88e37f54c271d257
AudiEroz <82683376+AudiEroz@users.noreply.github.com>
2021-04-17 01:53:54 +0300

    Add files via upload

---
+++ assets/js/audi11.js
@@ -0,0 +1,43 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQnyB466f8b7iT5bpyKLAkOtuo52124-g",
+    authDomain: "sadece-audi-challenge.firebaseapp.com",
+    projectId: "sadece-audi-challenge",
+    storageBucket: "sadece-audi-challenge.appspot.com",
+    messagingSenderId: "832788846818",
+    appId: "1:832788846818:web:1b88cf5b5ce1a7d01726bb"
+  };
+  // Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore()
+
+//Variable to access database collection
+const db = firestore.collection("audi11")
+
+//Get Submit Form
+let submitButton = document.getElementById('submit')
+
+//Create Event Listener To Allow Form Submission
+submitButton.addEventListener("click", (e) => {
+  //Prevent Default Form Submission Behavior
+  e.preventDefault()
+
+
+
+  //Get Form Values
+  let name = document.getElementById('name').value
+
+  //Save Form Data To Firebase
+  db.doc().set({
+    name: name,
+  }).then( () => {
+    console.log("Data saved")
+window.location.href = 'social.html';
+  }).catch((error) => {
+    console.log(error)
+  })
+
+  //alert
+
+
+})
